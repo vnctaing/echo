647d050bde01d289eaeeb1ac51ef9e2fc184c737
Vincent Taing <taing.vincent@gmail.com>
2014-08-15 18:46:31 +0200

    Ajout composer.json

---
+++ application/config/email.php
@@ -0,0 +1,15 @@
+<?php
+$config['useragent']    = 'CodeIgniter';
+$config['protocol']     = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.googlemail.com';
+$config['smtp_user']    = 'echoteam.mmi@gmail.com'; // Your gmail id
+$config['smtp_pass']    = 'echo123!'; // Your gmail Password
+$config['smtp_port']    = 465;
+$config['wordwrap']     = TRUE;
+$config['wrapchars']    = 76;
+$config['mailtype']     = 'html';
+$config['charset']      = 'iso-8859-1';
+$config['validate']     = FALSE;
+$config['priority']     = 3;
+$config['newline']      = "\r\n";
+$config['crlf']         = "\r\n";
