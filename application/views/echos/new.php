5ac8b2eb2ad59bbcbcaae668b626a15d289cc56f
Emma <emma.grisot@gmail.com>
2014-11-28 21:21:57 +0100

    Design premier jet

--- application/views/echos/new.php
+++ application/views/echos/new.php
@@ -2,36 +2,53 @@
 <head>
   <title>Accueil</title>
   <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/new.css"> <!-- Pour tous -->
+  <link rel="stylesheet" media="screen and (max-width: 640px)" href="../../assets/css/new-mobile.css" /> <!-- Pour mobiles (640px) -->
+
+
+
+  <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <div id="navbar">
     <a href="#"></a>
-    <div class="container">
-      <h1>Les paroles s'envolent, les écrits aussi.</h1>
-      <hr>
-      <h2>Echo est un service qui vous permet de créer un contenu éphémère, et de le partager avec vos amis.</h2>
-    </div>
     <ul>
       <li><?php echo anchor('main/login', 'Connexion'); ?></li>
       <li><?php echo anchor('main/signup', 'Inscription'); ?></li>
     </ul>
   </div>
+  <div id="leftcontent">
+    <h1> Les paroles s'envolent, les écrits aussi.</h1>
 
-  <div class="champecho">
+    <h2> Comment ça marche ?</h2>
+    <hr />
+    <br>
+    <p class="help">1. Créez votre contenu, votre echo, écrivez ce que vous voulez.
+    <br>2. Choisissez la durée de vie que vous voulez attribuer à ce contenu, 10 minutes ? 1h30 ?
+    <br>3. Générez le lien de votre echo, et partagez-le où vous le souhaitez.
+    <br>4. La durée de vie de votre echo augmentera si les visiteurs aiment le contenu et le font raisonner.
+    <br>5. À la fin de sa durée de vie, votre echo disparaîtra.</p>
+  </div>
+  <div id="rightcontent">
+    <h2> Créez votre contenu</h2>
+    <hr>
+    <br>
     <?php
       echo form_open('echos/create'); // Crée un formulaire qui appelle la méthode create du controlleur echos
       echo $this->session->flashdata('add_success');
       $attributes = array(
-        'placeholder' => 'Exprime-toi :)',
+        'placeholder' => 'Exprimez-vous !',
         'name' => 'content'
         );
       echo form_textarea($attributes);
     ?>
-  </div>
-  <div class="droite">
+    <br>
+    <br>
+    <h2> Choisissez votre durée de vie</h2>
+    <hr>
+    <br>
     <?php
       
-      echo 'Choisis la durée de vie :</br></br>';
       $options = array(
         5 => '5 min',
         10 => '10 min',
@@ -46,7 +63,7 @@
       echo form_dropdown('expired_at', $options, '1');
       echo 'Option Chiffrement: ';
       echo form_checkbox('encrypt', 1);
-      echo 'Entrez une clef secrete :';
+      echo 'Entrez une clé secrète :';
       echo form_input('secretkey');
     ?>
   
