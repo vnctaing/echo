45573c319d384e75c5b3872a956b9edaa6270248
Emma <emma.grisot@gmail.com>
2014-12-27 16:07:58 +0100

    404

---
+++ application/views/welcome/404_error.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE HTML>
+<html lang="fr" class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Erreur 404</title>
+  <meta name="description" content="">
+  <?=css('404.css')?>
+  <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'> 
+</head>
+
+<body>
+
+  <div id="content">
+    <div class="ksk"><p>4<img src="../../assets/img/O-blanc.png" alt=""/>4<p></div>
+    <h1>Il semble que vous êtes perdu</h1>
+    <h2>La page que vous recherchez n'existe pas...</h2>
+    <a href="../../../index.php">
+    <p class="back"><img id="plane" src="../../assets/img/Plane2.png"/>Retour à l'accueil</p>
+    </a>
+  </div>  
+</body>
+</html>
