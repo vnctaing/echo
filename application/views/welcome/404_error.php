4f6bf78b801e853d3ff7a26df50b4e8e2e2d7901
Emma <emma.grisot@gmail.com>
2014-12-27 19:54:56 +0100

    404 responsive

--- application/views/welcome/404_error.php
+++ application/views/welcome/404_error.php
@@ -7,6 +7,10 @@
   <title>Erreur 404</title>
   <meta name="description" content="">
   <?=css('404.css')?>
+  <link rel="stylesheet" type="text/css" media="screen and (max-width: 1000px)" href="../../assets/css/404-1000px.css">
+  <link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="../../assets/css/404-mobile.css">
+
+
   <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'> 
 </head>
 
@@ -14,8 +18,8 @@
 
   <div id="content">
     <div class="ksk"><p>4<img src="../../assets/img/O-blanc.png" alt=""/>4<p></div>
-    <h1>Il semble que vous êtes perdu</h1>
-    <h2>La page que vous recherchez n'existe pas...</h2>
+    <h1>Oh oh... Vous êtes perdu ?</h1>
+    <h2>La page que vous recherchez n'existe pas.</h2>
     <a href="../../../index.php">
     <p class="back"><img id="plane" src="../../assets/img/Plane2.png"/>Retour à l'accueil</p>
     </a>
