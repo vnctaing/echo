0c28a94ce9587f0871ce7882e4abbf68574f1245
Vincent Taing <taing.vincent@gmail.com>
2015-01-10 17:10:12 +0100

    Divers corrections css

--- application/views/echos/show.php
+++ application/views/echos/show.php
@@ -53,22 +53,19 @@
     <script type="text/javascript">
      window.twttr=(function(d,s,id){var t,js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);return window.twttr||(t={_e:[],ready:function(f){t._e.push(f)}})}(document,"script","twitter-wjs"));
     </script>
-    <div class="ajout">
-    <?php echo $this->session->flashdata('echo_success'); ?>
-    </div>
   </div>
+  <div class="ajout">
+    <?php echo $this->session->flashdata('echo_success'); 
+      echo $this->session->flashdata('errorDoubleRez');
+    ?>
+  </div>
+
   <div id="share">
     <a class="twitter-share-button"
       href="https://twitter.com/share"
       data-via="social_EchoFR">
     Tweet
     </a>
-  </div>
-
-  <div class="ajout">
-    <?php echo $this->session->flashdata('echo_success'); 
-      echo $this->session->flashdata('errorDoubleRez');
-    ?>
   </div>
 
 
