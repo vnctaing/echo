5658a1a41a569b72565f079f34b8f9f55f546a5c
Emma <emma.grisot@gmail.com>
2015-01-12 16:20:15 +0100

    ajout google analytics

--- application/views/echos/show.php
+++ application/views/echos/show.php
@@ -7,8 +7,16 @@
   <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" media="screen and (max-width: 800px)" href="../../assets/css/show-mobile.css">
   
-  <script language=javascript> var url= document.URL; </script> 
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-58489011-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
   <div id="navbar">
