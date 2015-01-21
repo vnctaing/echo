b8a9dcaef65aa86401bba70dcbfa6fcc52edcbda
Vincent Taing <taing.vincent@gmail.com>
2015-01-21 18:34:09 +0100

    Facebook SDK

--- application/views/echos/show.php
+++ application/views/echos/show.php
@@ -17,8 +17,26 @@
   ga('send', 'pageview');
 
 </script>
+
 </head>
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '903240606371281',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
   <div id="navbar">
   <?php echo anchor(base_url(), '<img src="../../assets/img/home.png" class="menu">'); ?>
   <?php echo anchor(base_url(), '<img src="../../assets/img/echo.png">'); ?>
