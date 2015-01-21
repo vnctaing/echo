a5dddc75eeba150c39ee8a935a0cd98cb0d0e39a
Emma <emma.grisot@gmail.com>
2015-01-21 16:22:21 +0100

    test, supprimer description en la remplacant

--- application/views/echos/show.php
+++ application/views/echos/show.php
@@ -90,7 +90,8 @@
         }(document, 'script', 'facebook-jssdk'));</script>
     <div class="fb-share-button" data-layout="button_count"></div>
     <meta property="og:image" content="../../assets/img/o-violet-icon.png" />
-    <meta property="og:title" content="Echo : les paroles s'envolent, les écrits aussi !" />
+    <meta property="og:title" content="Echo : les paroles s'envolent, les écrits aussi." />
+    <meta property="og:description" content="Créez votre contenu éphémère, à partager où vous le souhaitez !" /> 
 
     <form>
       <input class="share-url" onFocus="this.select()" name="share-url" value="<?php echo base_url($echo[0]->gkey); ?>">
